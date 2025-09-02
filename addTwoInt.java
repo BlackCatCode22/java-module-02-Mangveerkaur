5c5d1728b03910517b1952170ff6bfeab78d18f0
Mangveerkaur <kaurmangveer4@gmail.com>
2025-09-02 11:27:29 -0700

    Add files via upload

---
+++ addTwoInt.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 3;
+        int bye = 4;
+
+        int z = add(num1,bye);
+
+        System.out.println(z);
+    }
+    static int add(int num1, int bye) {
+
+        int z = num1 + bye;
+        return z;
+
+        }
+    }
